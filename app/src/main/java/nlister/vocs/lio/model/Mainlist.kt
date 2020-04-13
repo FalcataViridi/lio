c2ebd0c16ee8b9b2f77874d6815847a5de595522
Moises <falcataviridi@gmail.com>
2020-04-13 23:23:01 +0200

    definiendo las bases 2

--- app/src/main/java/nlister/vocs/lio/model/Mainlist.kt
+++ app/src/main/java/nlister/vocs/lio/model/Mainlist.kt
@@ -1,13 +1,21 @@
 package nlister.vocs.lio.model
+import java.util.*
 
 
-data class MainListItemData(val id: Long
-                                , var name: String = ""
-                                , val listType: String = ""
-                                , var description: String = ""
-                                , val status: String = "")
+data class MainListItemData(val id: String = Time("A")) {
+    var name: String? = ""
+    val listType: String? = ""
+    var description: String? = ""
+    val status: String? = ""
+}
 
-data class ShoppingListItemData(val id: Long
-                                    , var name: String = ""
-                                    , var cantidad: String = ""
-                                    , val status: String = "")
+data class ShoppingListItemData(val id: String = Time("S")) {
+    var name: String = ""
+    var cantidad: String = ""
+    val status: String = ""
+}
+
+fun Time(type: String): String {
+    return type + Date()
+}
+
